f67ba6558a519623a84d877568ca7de2cda3ad04
Elavarasan <elavarasan91827@gmail.com>
2024-09-24 19:35:09 +0530

    first commit

---
+++ keys.js
@@ -0,0 +1,6 @@
+//const mongoUrl = "mongodb+srv://Elavarasan:JluW9t2WirYIR1xh@cluster0.ly71i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+module.exports = {
+     mongoUrl : "mongodb+srv://Elavarasan:JluW9t2WirYIR1xh@cluster0.ly71i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    Jwt_secret : "passwordextra"
+};
---
+++ models/model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.SchemaType
+
+
+ 
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+
+
+    },
+    userName:{
+        type:String,
+        required:true
+    },
+
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    photo: {
+        type :String,
+    },
+    followers : [{ type: ObjectId,ref:"USER"}],
+    following :[{ type:ObjectId,ref:"USER"}]
+
+})
+
+mongoose.model("USER",userSchema)
